8a367a735c55b2f110cbd2659ddf02e2f4336fdf
hyeonjuuu <janghyeonjuu@gmail.com>
2024-03-22 11:48:11 +0900

    [Feat] TripReview 페이지 라우터 설정

--- src/components/tripreview/TripReviewFeed.tsx
+++ src/components/tripreview/TripReviewFeed.tsx
@@ -1,33 +1,42 @@
 import { ReviewDataTypes } from "@/types/ReviewDataTypes";
 import EnterButton from "./EnterButton";
+import { useRouter } from "next/router";
 
 interface TripReviewFeedProps {
   reviewData: ReviewDataTypes;
 }
 
 function TripReviewFeed({ reviewData }: TripReviewFeedProps) {
+  const router = useRouter();
+  const handleReviewDetail = (e: React.MouseEvent, id: Number) => {
+    router.push(`/tripreviewdetail/${id}`);
+  };
+
   return (
-    <a href="" className="flex flex-col w-100%  border-[#EDF2F2] border-[1px]">
+    <button
+      onClick={(e) => handleReviewDetail(e, reviewData.id)}
+      className="flex flex-col  w-100%  border-[#EDF2F2] border-[1px]"
+    >
       <img
         src={reviewData?.review_image[0]}
         alt=""
         className=" aspect-square object-cover"
       />
-      <div className="flex flex-col px-3">
+      <div className="flex flex-col px-3 w-full ">
         <span className="text-primary text-sm underline w-fit my-[26px] ">
           {reviewData.region === null ? "지역" : reviewData.region}
         </span>
-        <h3 className="text-3xl font-bold text-contentSecondary mb-2">
+        <h3 className="text-3xl font-bold text-contentSecondary mb-2 line-clamp-2 text-ellipsis overflow-clip self-start">
           {reviewData.title}
         </h3>
-        <span className="text-xs text-contentMuted font-light">
-          24.01.20 - 24.01.22
+        <span className="text-xs text-contentMuted font-light self-start">
+          {`${reviewData?.trip_date[0]} - ${reviewData?.trip_date[reviewData.trip_date.length - 1]}`}
         </span>
       </div>
-      <button className="self-end px-3 my-auto mb-7">
+      <div className="self-end px-3 my-auto mb-7">
         <EnterButton />
-      </button>
-    </a>
+      </div>
+    </button>
   );
 }
 
